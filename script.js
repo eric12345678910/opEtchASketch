04b7551009672e167dc013ce2ea296c8dfe322c5
eric12345678910 <ericlaudrum@gmail.com>
2024-09-09 21:37:14 -0400

    Update grid to 20x20 cells

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ console.log('etch-a-sketch!')
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    for (let i = 0; i < 16; i++){
+    for (let i = 0; i < 400; i++){
         let section = document.getElementById('sketchPad');
         let div = document.createElement('div');
         section.appendChild(div);
     }
-})
+});
