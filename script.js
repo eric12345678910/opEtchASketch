c5931d91933c50010b11cb93b4ed4b218e402ec6
eric12345678910 <ericlaudrum@gmail.com>
2024-09-09 22:03:00 -0400

    Add mouseover line drawing and reset button

--- script.js
+++ script.js
@@ -5,6 +5,28 @@ document.addEventListener('DOMContentLoaded', () => {
     for (let i = 0; i < 400; i++){
         let section = document.getElementById('sketchPad');
         let div = document.createElement('div');
+        div.className = 'sketchCell';
         section.appendChild(div);
     }
+
+    const cells = document.querySelectorAll('.sketchCell');
+
+    cells.forEach(cell => {
+        cell.addEventListener('mouseover', (e) => {
+            cell.style.backgroundColor = 'black';
+        });
+    });
+
+    const reset = document.getElementById('reset');
+    reset.addEventListener('click', () => {
+        cells.forEach(cell => {
+            cell.style.backgroundColor = 'antiquewhite';
+        })
+    })
+
+
+
+
+
 });
+
